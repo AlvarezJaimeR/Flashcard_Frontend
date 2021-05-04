66ef778816cd230e0b02ab3208b198293416370e
Jaime Alvarez <jrralvarez@gmail.com>
2021-05-03 21:44:37 -0400

    able to get the collections from the backend. added a loading screen prior to any data from loading

--- flashcard/src/components/app.jsx
+++ flashcard/src/components/app.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TitleBar from './TitleBar/titleBar';
 
 class App extends Component {
-    state = { }
+    constructor(props){
+        super(props);
+        this.state = {
+            flashcardCollection: [],
+            loading: true
+        }
+    }
+
+    componentDidMount(){
+        axios.get("http://localhost:5000/api/collections")
+        .then(res => {
+            console.log(res);
+            const collections = res.data;
+            this.setState({flashcardCollection:collections, loading: false})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
     render() {
         return (
-            <h1>Our React App Using a Component</h1>
+            this.state.loading ? <div>loading...</div>:
+            <div className = "container-fluid">
+                {console.log(this.state.flashcardCollection)}
+                <TitleBar />
+            </div>
         );
     }
 }
